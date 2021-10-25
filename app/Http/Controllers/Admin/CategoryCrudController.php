43816bd134d7e1e984296fd1e8aa2266d60d8169
phamquanglinhdev <phamquanglinhdev@gmail.com>
2021-10-25 10:27:54 +0700

    add thumbnail to course

--- app/Http/Controllers/Admin/CategoryCrudController.php
+++ app/Http/Controllers/Admin/CategoryCrudController.php
@@ -21,7 +21,7 @@ class CategoryCrudController extends CrudController
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     * 
+     *
      * @return void
      */
     public function setup()
@@ -33,43 +33,45 @@ class CategoryCrudController extends CrudController
 
     /**
      * Define what happens when the List operation is loaded.
-     * 
+     *
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
     protected function setupListOperation()
     {
-        CRUD::setFromDb(); // columns
+        CRUD::addColumn(['name' => 'name', 'type' => 'text']);
+        CRUD::addColumn(['name' => 'thumbnail', 'type' => 'image']);
 
         /**
          * Columns can be defined using the fluent syntax or array syntax:
          * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
          */
     }
 
     /**
      * Define what happens when the Create operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
     protected function setupCreateOperation()
     {
         CRUD::setValidation(CategoryRequest::class);
+        CRUD::addField(['name' => 'name', 'type' => 'text']);
+        CRUD::addField(['name' => 'thumbnail', 'type' => 'image']);
 
-        CRUD::setFromDb(); // fields
 
         /**
          * Fields can be defined using the fluent syntax or array syntax:
          * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
          */
     }
 
     /**
      * Define what happens when the Update operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,19 +15,23 @@ class CategorySeeder extends Seeder
     public function run()
     {
         $data = [
-            "name"=>"C++"
+            "name"=>"C++",
+            "thumbnail"=>"https://webdanhgia.vn/wp-content/uploads/2021/04/danh-gia-ngon-ngu-lap-trinh-C.jpg"
         ];
         Category::create($data);
         $data = [
-            "name"=>"Python"
+            "name"=>"Python",
+            "thumbnail"=>"https://hoadm.net/wp-content/uploads/2020/05/python-logo-741x486.jpg"
         ];
         Category::create($data);
         $data = [
-            "name"=>"Custom"
+            "name"=>"Logic",
+            "thumbnail"=>"https://thichgiaiphap.com/wp-content/uploads/2021/04/06-4.png"
         ];
         Category::create($data);
         $data = [
-            "name"=>"Backend"
+            "name"=>"Backend",
+            "thumbnail"=>"https://cuongteam.com/wp-content/uploads/2021/08/nhap-mon-laravel.png"
         ];
         Category::create($data);
     }
